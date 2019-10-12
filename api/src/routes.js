1de96b9ad6d204d422798333281214dc1f79ee5e
lucascarrafa <lucascarrafa@hotmail.com>
2019-10-12 11:19:58 -0300

    novas rotas

--- api/src/routes.js
+++ api/src/routes.js
@@ -5,6 +5,8 @@ const routes = express.Router()
 const SNMPSwitch = require('./controllers/SNMPSwitch')
 
 routes.get('/nome',SNMPSwitch.nome)
+routes.get('/porta01',SNMPSwitch.porta01)
+routes.get('/porta02',SNMPSwitch.porta02)
 
 module.exports = routes
 
