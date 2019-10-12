57214be717060377d695da8be7025cc07ec0ac9c
lucascarrafa <lucascarrafa@hotmail.com>
2019-10-12 15:34:38 -0300

    conexao com o banco de dados usando mongoose

--- api/src/server.js
+++ api/src/server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const routes = require('./routes')
+const mongoose = require('mongoose')
 
 const server = express() //cria um servidor
+
+mongoose.connect('mongodb://localhost:27017/desafio', {useUnifiedTopology: true,  useNewUrlParser: true })
 
 server.use(express.json())
 
