0dcd5454cd9c8e9f91bd6576de5933ca8cea768a
lucascarrafa <lucascarrafa@hotmail.com>
2019-10-14 14:02:51 -0300

    inserção de novas rotas deletar, inserir e atualizar

--- api/src/routes.js
+++ api/src/routes.js
@@ -8,6 +8,8 @@ routes.get('/',SNMPSwitch.raiz)
 routes.get('/nome',SNMPSwitch.nome)
 routes.get('/porta01',SNMPSwitch.porta01)
 routes.get('/porta02',SNMPSwitch.porta02)
+routes.post('/inserir',SNMPSwitch.inserir)
+routes.post('/atualizar',SNMPSwitch.atualizar)
+routes.post('/deletar',SNMPSwitch.deletar)
 
 module.exports = routes
-
